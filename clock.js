9fe82a6fb5ce9b096f8e2cb6cb859e9ac22f087b
Priyal2807 <priyalsobti@gmail.com>
2018-01-29 23:01:23 +0530

    Javascript Wesbos 30

--- clock.js
+++ clock.js
@@ -8,11 +8,11 @@ function setDate(){
   const secondsDegrees = ((seconds / 60) * 360) + 90;
  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   const minutes=now.getMinutes();
-  const minuteDegress=((minutes/60)*360)+90+((seconds/60)*6);
+  const minuteDegrees=((minutes/60)*360)+90+((seconds/60)*6);
   minuteHand.style.transform=`rotate(${minuteDegrees}deg)`;
 
   const hour=now.getHours();
-  const hourDegrees=((hour/12)*360)+90+((minutes/60)*30);
+  const hourDegrees=((hour / 12)*360)+90+((minutes / 60)*30);
   hourHand.style.transform=`rotate(${hourDegrees}deg)`;
 }
 setInterval(setDate,1000);
