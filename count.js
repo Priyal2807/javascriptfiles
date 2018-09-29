1da754d0d523b1d72fa42eefbf2b97bf86eb46ad
Priyal Sobti <priyalsobti@gmail.com>
2018-09-29 22:41:57 +0530

    learning node.js

---
+++ count.js
@@ -0,0 +1,12 @@
+module.exports.count=function(arr){
+  return arr.length;
+}
+module.exports.adder=function(a,b){
+  return `The sum of number is ${a+b}`
+}
+module.exports.str="Yoho cool"; //module.exports is an empty object and we can set any type of properties we want
+/*module.exports={
+count:count,
+adder:adder,
+str:str
+};*/
